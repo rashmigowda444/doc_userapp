e893140ed5348f7fdc2258055b6ecf4ed889ad91
shwethashree <shwethashreen07@gmail.com>
2018-05-21 12:15:11 +0530

    new user app

--- userapp/src/app/app.module.ts
+++ userapp/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-
+import { FormsModule ,ReactiveFormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const appRoutes:Routes=[
   {path:'', component:HomeComponent},
-  {path:'home', component:HomeComponent}
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'activities', component:ActivitiesComponent},
+  {path:'contact', component:ContactComponent}
 ]
 
 @NgModule({
@@ -17,13 +23,18 @@ const appRoutes:Routes=[
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ActivitiesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppBootstrapModule{} 
